45abd7081e11fd7a173b3d607d7a4b2ad5f23e85
ralph950412 <ralph950412@gmail.com>
2026-06-24 20:22:27 +0900

    sm8250-common: Add libaudiobase dependency to libwfdmmsrc_system
    AudioSystem and its symbols have been moved to libaudiobase.
    
    Reference:
    https://android.googlesource.com/platform/frameworks/av/+/24d4f12702b932e25f9b8585e0e9120cd242ce94

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
