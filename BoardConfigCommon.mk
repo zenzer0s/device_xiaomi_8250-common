c3f339c2df6aa1cdfa697843dd44eb9dcfd3b613
zenzer0s <praveenzalaki200@gmail.com>
2026-06-25 17:32:19 +0000

    sm8250-common: Migrate TARGET_CAMERA_PACKAGE_NAME to soong_config_set

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,7 +73,7 @@ TARGET_NO_BOOTLOADER := true
 MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Camera - Miui
-TARGET_CAMERA_PACKAGE_NAME := com.android.camera
+$(call soong_config_set,camera,package_name,com.android.camera)
 
 # Camera - Torch
 TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8250
